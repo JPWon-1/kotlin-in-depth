e477db4f22a3b38c1aa7d08a75eead4249341b91
JP Won <rocknzero1@gmail.com>
2022-08-21 20:48:53 +0900

    2장 코틀린 기본 문법 공부

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,14 @@
 fun main(){
-    println("Hello, KotlinVerse!")
+    val a = readLine()!!.toInt()
+    val b = readLine()!!.toInt()
+    println(a+b)
+
+    val squaresA = Array(a){(it+1)*(it+1)}
+    squaresA.iterator().forEach {
+        println(it)
+    }
+    val squaresB = Array(b){(it+1)*(it+1)}
+    squaresB.iterator().forEach {
+        println(it)
+    }
 }
